09a7cc362d60e72862b92f78ace3d0dcec6cf3f1
thelast7 <takimanwar@gmail.com>
2019-01-16 11:14:31 +0700

    form lagi

--- src/fromNet/state/NetState.js
+++ src/fromNet/state/NetState.js
@@ -10,11 +10,26 @@ class NetState extends React.Component {
         alert('dont')
     }
 
+    handleChange = (e) => {
+        this.setState({
+            name : e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('form submitted', this.state.name)
+    }
+
     render() {
         return (
             <div>
-                <h1>nama {this.state.name} umur {this.state.umur}</h1>
-                <p onCopy={this.handleCopy}>awkdawk dkawawdkawk akwdkawdk wakdkawdkaw kdaw</p>
+                <form onSubmit={this.handleSubmit}>
+                    <h1>nama {this.state.name} umur {this.state.umur}</h1>
+                    <p onCopy={this.handleCopy}>awkdawk dkawawdkawk akwdkawdk wakdkawdkaw kdaw</p>
+                    <input type="text" onChange={this.handleChange}></input>
+                    <button>submit</button>
+                </form>
             </div>
         )
     }
